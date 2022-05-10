f05510ed4503893811645b2b39071cac550bb35b
Arminas19 <armeska14@gmail.com>
2022-05-10 18:22:07 +0000

    set-up my views in my about app, started on my contact_success.html page.

--- about/urls.py
+++ about/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.about_content, name='about'),
+    path('contact_success/', views.contact_success, name='contact_success'),
 ]
--- about/views.py
+++ about/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
 
+from .models import Contact
 # Create your views here.
 
 def about_content(request):
     """ A view to return the About page. """
+    if request.method == 'POST':
+        contact = Contact()
+
+        full_name = request.POST.get('full_name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        
+        contact.full_name = full_name
+        contact.email = email
+        contact.subject = subject
+        contact.message = message
+        contact.save()
+        return redirect(reverse('contact_success'))
+
     return render(request, 'about/about.html')
+
+
+def contact_success(request):
+    return render(request, 'about/contact_success.html', context=None)
