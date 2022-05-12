54bc67359ba58d3ee6ec622f428bcb357cb2a255
Arminas19 <armeska14@gmail.com>
2022-05-12 11:34:27 +0000

    set-up products views/models and admin

--- products/views.py
+++ products/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 
 def all_products(request):
     """ A view to return the products page. """
-    return render(request, 'products/products.html')
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
