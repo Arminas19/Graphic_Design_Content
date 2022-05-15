7a40232925e2d59833308a57a7ad9d217ade923c
Arminas19 <armeska14@gmail.com>
2022-05-15 13:00:51 +0000

    Started on the bag template, view and contexts.py file

--- bag/views.py
+++ bag/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def bag(request):
+def view_bag(request):
     """ A view to return the bag page. """
     return render(request, 'bag/bag.html')
