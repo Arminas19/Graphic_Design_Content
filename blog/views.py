ef903358a1e8a4edbf731f832ecef44f017d3c4e
Arminas19 <armeska14@gmail.com>
2022-06-01 15:48:01 +0000

    Started on the create_blog view and template

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import blog
+from .forms import BlogForm
+
 # Create your views here.
 
 
 def all_blogs(request):
     """  Blogs """
-    blogs = blog.objects.all()
 
+    blogs = blog.objects.all()
     context = {
         'blogs': blogs,
     }
@@ -14,6 +18,24 @@ def all_blogs(request):
     return render(request, 'blog/blogs.html', context)
 
 
+@login_required
 def create_blog(request):
     """  Create blog """   
-    return render(request, 'blog/create_blog.html')
+    blogs = blog.objects.get(author__id=request.user.id)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, instance=blogs)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully posted Bolg!')
+            return redirect(reverse('blogs'))
+        else:
+            messages.error(request, 'Failed to post blog, please ensure the form is valid.')
+    else:
+        form = BlogForm()
+        
+    template = 'blog/create_blog.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
