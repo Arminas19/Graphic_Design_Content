b9d9573f3cb70a90e62467b460ef2a58bc1bb4a9
Arminas19 <armeska14@gmail.com>
2022-06-01 09:51:52 +0000

    started on my blogs app and added a link to my blog template from my base.html header

--- Creative_design/settings.py
+++ Creative_design/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'about',
     'bag',
     'profiles',
+    'blog',
     'checkout',
     'crispy_forms',
 ]
---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class blog(models.Model):
+    title = models.CharField(max_length=150, null=False, blank=False)
+    body = models.TextField(max_length=2000, null=False, blank=False)
+    image = models.ImageField(upload_to='blogimages', null=True, blank=True)
+    date_published = models.DateTimeField(auto_now_add=True,
+                                          verbose_name="date published")
+    date_updated = models.DateTimeField(auto_now=True,
+                                        verbose_name="date updated")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-date_published']
+
+    def __str__(self):
+        return self.title
