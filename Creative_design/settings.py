527153cee69131cef87bc4d6c61587cd4ff55278
Arminas19 <armeska14@gmail.com>
2022-06-07 09:31:23 +0000

    Deployment part 2

--- Creative_design/settings.py
+++ Creative_design/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure--_n+*qcbrzc$kr91eau227ssyqtucu3m)g5%*7va-foylna-8#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['graphic-design-project.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,18 @@ WSGI_APPLICATION = 'Creative_design.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn Creative_design.wsgi:application
